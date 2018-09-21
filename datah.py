bf93bee73e1f2c7a0e7625f9b72a43e204efbc1f
Lyu-lyu <for_lyuda@inbox.ru>
2018-09-21 08:29:32 +0300

    initial commit

--- datah.py
+++ datah.py
@@ -7,12 +7,17 @@ from datetime import time
 
 
 def month_days(year, month):
+	# list of the days in months 
     m_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    # to get index in list we minus 1
     month_index = month - 1
     result = m_days[month_index]
+    # finding how many days february lasts
     if 1 == month_index:
+    	# checking if it is a leap-year
         if not (year % 4 != 0 or (year % 100 == 0 and year % 400 != 0)):
             result += 1
+    # returning 28 if it isn't a leap-year
     return result;
  
 now = datetime.now()
@@ -25,6 +30,7 @@ date_n_days_ago = now - d
 print('Today is {0}  month ago it was: {1}'.format(now, date_n_days_ago))
 
 date_string = '01/01/17 12:10:03.234567'
+# the second strftime defines the format date output
 date_dt = (datetime.strptime(date_string, '%d/%m/%y %H:%M:%S.%f')).strftime('%d/%m/%y %H:%M:%S.%f')
 print ('this date is {0}'.format(date_dt))
 
